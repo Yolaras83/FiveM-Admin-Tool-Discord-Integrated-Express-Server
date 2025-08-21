96502e14ef6babcc0df3cf40c8714c9e2b561980
Yolaras83 <94140646+Yolaras83@users.noreply.github.com>
2025-08-21 16:45:30 +0300

    Update index.js

--- Express Server/index.js
+++ Express Server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Client, IntentsBitField, EmbedBuilder } = require('discord.js');
+const { Client, IntentsBitField, EmbedBuilder, ChannelType } = require('discord.js');
 const cors = require('cors');
 
 const app = express();
@@ -7,9 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-
-const GUILD_ID = 'enter your guild id';
-const TOKEN = 'enter your token';
 
 const client = new Client({
     intents: [
@@ -22,14 +19,16 @@ const client = new Client({
     ],
 });
 
+const staffRole = '1286365936059940875';
+let supportChannel;
+
 app.post('/send-message', async(req, res) => {
     console.log('Received');
     const { discordId, message, adminName } = req.body;
     
     try {
-        const guild = await client.guilds.cache.get(GUILD_ID);
+        const guild = await client.guilds.cache.get('1286365306767540384');
         const member = await guild.members.cache.get(discordId);
-        console.log(discordId);
         const messageEmbed = new EmbedBuilder().setAuthor({ name: adminName}).setColor('Red').setDescription(message).setTitle('Message from Admin: ' + adminName).setTimestamp();
         await member.send({
             embeds: [messageEmbed],
@@ -40,11 +39,57 @@ app.post('/send-message', async(req, res) => {
     }
 });
 
+app.post('/create-support', async(req, res) => {
+    const discordId = req.body.discordId;
+
+    try {
+        const guild = await client.guilds.cache.get('1286365306767540384');
+        const member = await guild.members.cache.get(discordId); 
+        supportChannel = await guild.channels.create({
+            name: `📞Support-${member.user.tag}`,
+            parent: '1286366197058764901',
+            type: ChannelType.GuildVoice,
+            permissionOverwrites: [
+                {
+                    id: guild.roles.everyone.id,
+                    deny: ['ViewChannel'],
+                },
+                {
+                    id: discordId,
+                    allow: ['ViewChannel', 'Connect'],
+                },
+                {
+                    id: staffRole,
+                    allow: ['ViewChannel', 'Connect'],
+                },
+            ],
+        });
+
+        await member.send(`**A support voice room has been created for you by an admin! Please join it by pressing here: <#${supportChannel.id}>**`);
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({ error: "Error creating support room!" });
+    }
+});
+
 client.on('ready', async(c) => {
     console.log('logged in as bot');
 });
 
-client.login(TOKEN);
+client.on('voiceStateUpdate', async(oldState, newState) => {
+
+    if(!oldState.channel) return; 
+
+    if(!supportChannel) return;
+
+    if(oldState.channel.id == supportChannel.id) {
+        if(oldState.channel.members.size == 0) {
+            await oldState.channel.delete();
+        }
+    }
+});
+
+client.login('MTQwODA1MzY2OTAzMDUzMTExMg.G3tHG3.KKhoIStiqcVgdaQ6wZz519eCaMROcM6E9LqfgY');
 
 app.listen(3000, () => {
     console.log('Server started on http://localhost:3000');
